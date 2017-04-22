a7cff90a5fc82f41377f5193bb626f5467e6f230
Clearkeyboard <byron.clearkeyboard@gmail.com>
2017-04-22 12:30:06 -0400

    spotify and OMDB funcationlity added

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 console.log('this is loaded');
 
 exports.twitterKeys = {
@@ -11,18 +11,4 @@ exports.twitterKeys = {
 exports.spotifyKeys = {
   client_key: 'eaa691a91d2640d6826ce3199cfd1214',
   client_secret: '895cb2df48c44a2c9a1a236b597a8123',
-=======
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: '1HP1jnW36Zb1uW1JvriyyBLrC',
-  consumer_secret: '43lH0XpTVaz6ZT16ULrrwP7zF3m1kFYSfkD9xRnOyl3OsuDF6Q',
-  access_token_key: '2879751502-pHefVOoB3s5bYnjKz8DHEzRBOSdDiYDYd0PFpbt',
-  access_token_secret: 'HoVTeuVBs97s1DmE45FZOC9kQswujAVIg7k6Vd9QZIVZr',
-}
-
-exports.spotifyKeys = {
-  client_key: 'eaa691a91d2640d6826ce3199cfd1214',
-  client_secret: '895cb2df48c44a2c9a1a236b597a8123',
->>>>>>> eb94ffd85ab782d7e00261e604f14daa0e03f517
 }
--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var keys = require("./keys.js");
 //Grabs API keys from file
 var request = require("request");
@@ -6,8 +6,6 @@ var request = require("request");
 var fs = require("fs");
 //Grabs fs node construct
 
-
-var spot = keys.spotifyKeys;
 var input = process.argv[2];
 
 //Switch case for querys
@@ -39,10 +37,57 @@ function tweeting(){
     client.get('statuses/user_timeline', params, function(error, tweets, response){
         if (!error){
             console.log(JSON.parse(response.body));
-        }
+        }else{console.log("Twitter User Not Found or No Tweets to show")}
     });
 };
-
+//Grabs spotify track info
 function spotify(){
+    //Spotifiy API keys
+    var spot = keys.spotifyKeys;
+    //Argument for song query
     var song = process.argv[3];
-    var queryURL = 'https://api.spotify.com/v1/search
+    //Spotify Node Construct
+    var spotify = require('spotify');
+
+    //Spotify search NPM
+    spotify.search({type:'track', query: song}, function(err, data){
+        if(!error){
+        var artist = data.tracks.items[0].artists[0].name;
+        var title = data.tracks.items[0].name;
+        var album = data.tracks.items[0].album.name;
+        console.log(artist);
+        console.log(title);
+        console.log(album);
+        }else{console.log("Spotify Error or No Track Found")}
+    })
+}
+
+// OMDb API function
+function movie(){
+    var query = process.argv[3];
+    var queryURL = "http://www.omdbapi.com/?t=" + query + "&r=json&tomatoes=true";
+    request(queryURL, function (error, response, data){
+         if (!error && response.statusCode === 200) {
+             //Variables for storing nodes of JSON object. Not really necessary
+            var title = JSON.parse(data).Title;
+            var year = "Released: " + JSON.parse(data).Year;
+            var imdb = "IMDB Rating: " + JSON.parse(data).imdbRating;
+            var country = "Produced in " + JSON.parse(data).Country;
+            var language = "Languages: " + JSON.parse(data).Language;
+            var plot = "Plot Synopsis " + JSON.parse(data).Plot;
+            var actors = "Starring: " + JSON.parse(data).Actors;
+            var tomato = "More Info: " + JSON.parse(data).tomatoURL;
+            //Prints JSON nodes
+            console.log(title);
+            console.log(year);
+            console.log(imdb);
+            console.log(country);
+            console.log(language);
+            console.log(plot);
+            console.log(actors);
+            console.log(tomato);
+
+    }else{console.log(error)}
+});
+}
+
