f3ed51de7f987da9285d54492f04e86cb4c8339b
Clearkeyboard <byron.clearkeyboard@gmail.com>
2017-04-22 10:37:02 -0400

    initial commit

--- liri.js
+++ liri.js
@@ -4,17 +4,15 @@ var request = require("request");
 //Grabs Request node construct
 var fs = require("fs");
 //Grabs fs node construct
-var twitter = require('twitter');
-//Grabs Twitter node construct
-var spot = keys.spotifyKeys;
-var twitt = keys.twitterKeys;
-var input = process.argv[2];
 
+
+var spot = keys.spotifyKeys;
+var input = process.argv[2];
 
 //Switch case for querys
 switch (input){
     case "my-tweets":
-    twitter();
+    tweeting();
     break;
 
     case "spotify-this-song":
@@ -30,13 +28,21 @@ switch (input){
     break;
 }
 //Twitter query grabbing 20 tweets from user
-function twitter(){
-    var client = new twitter(keys.twitterKeys);
-    //stores API keys in a variable or use with function
-
-    client.get('search/tweets', {q:'clearkeyboard'}, function(error, tweets, response) {
-        if(error) throw error;
-        console.log(tweets);
-        console.log(response);
+function tweeting(){
+    var twitter = require('twitter');
+//Grabs Twitter node construct
+    var twitt = keys.twitterKeys;
+//Grabs Twitter keys Object
+    var client = new twitter(twitt);
+    var params = {screen_name: 'Clearkeyboard', count: 20};
+    client.get('statuses/user_timeline', params, function(error, tweets, response){
+        if (!error){
+            console.log(JSON.parse(response.body));
+        }
     });
+};
+
+function spotify(){
+    var song = process.argv[3];
+    var queryURL = 'https://api.spotify.com/v1/search
 }
